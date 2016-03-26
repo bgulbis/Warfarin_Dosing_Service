1ca37842f30ec96486cb75594ac1e28f33541bf7
bgulbis <brian.gulbis@gmail.com>
2016-03-26 18:35:00 -0500

    compare charges with admin records

--- 1-screen.R
+++ 1-screen.R
@@ -2,3 +2,13 @@
 
 source("0-library.R")
 
+pts.charge <- read_edw_data(patient.dir, "patients_warfarin", "charges",
+                            check.distinct = FALSE) %>%
+    distinct(pie.id)
+
+pts.events <- read_edw_data(patient.dir, "patients_events", "patients",
+                            check.distinct = FALSE) %>%
+    distinct(pie.id)
+
+pts <- full_join(pts.charge, pts.events, by = "pie.id")
+
