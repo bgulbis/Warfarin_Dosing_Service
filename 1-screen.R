efd78e0db75c5b52407df41df9c6fe4ba2667d7f
bgulbis <brian.gulbis@gmail.com>
2016-04-12 08:49:45 -0500

    get warfarin doses, remove patients with < 3 doses

--- 1-screen.R
+++ 1-screen.R
@@ -8,3 +8,11 @@ raw.patients <- read_edw_data(patient.dir, "patients_events_warfarin", "patients
 
 edw.pie <- concat_encounters(raw.patients$pie.id, 750)
 print(edw.pie)
+
+raw.warfarin <- read_edw_data(patient.dir, "meds_sched_warfarin", "meds_sched")
+
+# only include patients who received at least 3 doses
+tmp.include <- raw.warfarin %>%
+    group_by(pie.id) %>%
+    summarize(count = n()) %>%
+    filter(count > 2)
