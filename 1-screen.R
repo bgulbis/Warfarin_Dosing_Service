ee3a687dca737d55dd47a7c45429744f0147ad1d
bgulbis <brian.gulbis@gmail.com>
2016-04-12 10:41:54 -0500

    apply inclusion criteria

--- 1-screen.R
+++ 1-screen.R
@@ -8,7 +8,6 @@ raw.patients <- read_edw_data(dir.patients, "patients_events_warfarin", "patient
            discharge.datetime < mdy("1/1/2016"))
 
 edw.pie <- concat_encounters(raw.patients$pie.id, 750)
-print(edw.pie)
 
 # get total number of patients per year
 analyze.patients.all <- raw.patients %>%
@@ -20,7 +19,35 @@ raw.warfarin <- read_edw_data(dir.patients, "meds_sched_warfarin", "meds_sched")
     semi_join(raw.patients, by = "pie.id")
 
 # only include patients who received at least 3 doses
-tmp.include <- raw.warfarin %>%
+pts.include <- raw.warfarin %>%
     group_by(pie.id) %>%
     summarize(count = n()) %>%
     filter(count > 2)
+
+edw.pie <- concat_encounters(pts.include$pie.id, 750)
+
+# only inlcude patients with a baseline INR < 1.5 (last prior to warfarin start)
+tmp.warfarin.start <- raw.warfarin %>%
+    semi_join(pts.include, by = "pie.id") %>%
+    group_by(pie.id) %>%
+    arrange(med.datetime) %>%
+    summarize(warf.start = first(med.datetime))
+
+raw.inr <- read_edw_data(dir.patients, "warfarin_coags", "labs") %>%
+    semi_join(pts.include, by = "pie.id") %>%
+    filter(lab == "inr")
+
+pts.include <- raw.inr %>%
+    inner_join(tmp.warfarin.start, by = "pie.id") %>%
+    filter(lab.datetime <= warf.start) %>%
+    group_by(pie.id) %>%
+    arrange(lab.datetime) %>%
+    mutate(inr.time.prior = as.numeric(difftime(last(lab.datetime),
+                                                warf.start, units = "days"))) %>%
+    summarize(inr.baseline = last(lab.result),
+              inr.time.prior = last(inr.time.prior)) %>%
+    filter(inr.baseline < 1.5)
+
+edw.pie <- concat_encounters(pts.include$pie.id, 750)
+
+print(edw.pie)
