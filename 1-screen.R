fdef88e6410b017dfb534297e5fab94d8e6a5d91
bgulbis <brian.gulbis@gmail.com>
2016-04-12 10:07:25 -0500

    get number of warfarin patients by year

--- 1-screen.R
+++ 1-screen.R
@@ -3,13 +3,21 @@
 source("0-library.R")
 
 # get list of patients receiving warfarin
-raw.patients <- read_edw_data(patient.dir, "patients_events_warfarin", "patients") %>%
-    filter(age >= 18)
+raw.patients <- read_edw_data(dir.patients, "patients_events_warfarin", "patients") %>%
+    filter(age >= 18,
+           discharge.datetime < mdy("1/1/2016"))
 
 edw.pie <- concat_encounters(raw.patients$pie.id, 750)
 print(edw.pie)
 
-raw.warfarin <- read_edw_data(patient.dir, "meds_sched_warfarin", "meds_sched")
+# get total number of patients per year
+analyze.patients.all <- raw.patients %>%
+    mutate(year.cal = year(discharge.datetime))
+
+save_rds(dir.analysis, "analyze")
+
+raw.warfarin <- read_edw_data(dir.patients, "meds_sched_warfarin", "meds_sched") %>%
+    semi_join(raw.patients, by = "pie.id")
 
 # only include patients who received at least 3 doses
 tmp.include <- raw.warfarin %>%
