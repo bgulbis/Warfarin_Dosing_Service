da423012385bed4b5afc621bab74123919840efb
bgulbis <brian.gulbis@gmail.com>
2016-04-22 10:28:36 -0500

    make list of person id's

--- 3-tidy.R
+++ 3-tidy.R
@@ -4,6 +4,8 @@ source("0-library.R")
 
 tmp <- get_rds(dir.tidy)
 
-# data from exclude dir: warfarin goals, LFTs
+tidy.demographics <- read_edw_data(dir.data, "demographics") %>%
+    inner_join(pts.include, by = "pie.id")
 
-raw.demographics <- read_edw_data(dir.data, "demographics")
+edw.persons <- concat_encounters(tidy.demographics$person.id, 750)
+print(edw.persons)
