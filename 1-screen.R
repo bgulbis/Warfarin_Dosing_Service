effccf7f85b04b01e39f87c00d81cb69fa116f08
bgulbis <brian.gulbis@gmail.com>
2016-05-24 08:56:55 -0500

    temporarily remove code for individual warfarin courses

--- 1-screen.R
+++ 1-screen.R
@@ -3,7 +3,7 @@
 source("0-library.R")
 
 # get list of patients receiving warfarin
-raw.patients <- read_edw_data(dir.patients, "patients_events_warfarin", "patients") %>%
+raw.patients <- read_edw_data(dir.patients, "patients") %>%
     filter(age >= 18,
            discharge.datetime < mdy_hm("1/1/2016 00:00"))
 
@@ -14,47 +14,46 @@ edw.pie.all <- concat_encounters(raw.patients$pie.id, 900)
 analyze.patients.all <- raw.patients %>%
     mutate(year.cal = year(discharge.datetime))
 
-raw.warfarin <- read_edw_data(dir.patients, "meds_sched_warfarin", "meds_sched") %>%
+raw.warfarin <- read_edw_data(dir.patients, "meds_sched") %>%
     semi_join(raw.patients, by = "pie.id")
 
 # identify unique warfarin courses; criteria are first dose in hospital, more
 # than 5 days since prior warfarin dose, or 2-4 since last warfarin dose and the
 # INR is < 1.5 during this time
-tmp.warfarin.doses <- raw.warfarin %>%
-    mutate(dose.date = floor_date(med.datetime, unit = "day")) %>%
-    group_by(pie.id, dose.date) %>%
-    summarize(med.dose = sum(med.dose),
-              num.dose = n()) %>%
-    group_by(pie.id) %>%
-    mutate(days.prev = as.numeric(difftime(dose.date, lag(dose.date), units = "days")))
-
-tmp.dates <- tmp.warfarin.doses %>%
-    group_by(pie.id) %>%
-    summarize(first.date = first(dose.date),
-              last.date = last(dose.date))
-
-tmp.inr <- read_edw_data(dir.patients, "inr", "labs") %>%
-    mutate(inr.date = floor_date(lab.datetime, unit = "day"),
-           lab.result = as.numeric(lab.result)) %>%
-    inner_join(tmp.dates, by = "pie.id") %>%
-    filter(inr.date >= first.date,
-           inr.date <= last.date) %>%
-    group_by(pie.id, inr.date) %>%
-    summarize(inr = max(lab.result))
-
-# convert NA's to 0 before using cumsum function
-cumsum_na <- function(x) {
-    x[is.na(x)] <- 0
-    cumsum(x)
-}
-
-data.warfarin.courses <- tmp.warfarin.doses %>%
-    full_join(tmp.inr, by = c("pie.id", "dose.date" = "inr.date")) %>%
-    arrange(pie.id, dose.date) %>%
-    mutate(new.dose = ifelse((!is.na(med.dose) & is.na(days.prev)) | days.prev > 5 |
-                                 (days.prev > 2 & lag(inr) < 1.5), TRUE, FALSE),
-           course = cumsum_na(new.dose))
-
+# tmp.warfarin.doses <- raw.warfarin %>%
+#     mutate(dose.date = floor_date(med.datetime, unit = "day")) %>%
+#     group_by(pie.id, dose.date) %>%
+#     summarize(med.dose = sum(med.dose),
+#               num.dose = n()) %>%
+#     group_by(pie.id) %>%
+#     mutate(days.prev = difftime(dose.date, lag(dose.date), units = "days"))
+#
+# tmp.dates <- tmp.warfarin.doses %>%
+#     group_by(pie.id) %>%
+#     summarize(first.date = first(dose.date),
+#               last.date = last(dose.date))
+#
+# tmp.inr <- read_edw_data(dir.patients, "inr", "labs") %>%
+#     mutate(inr.date = floor_date(lab.datetime, unit = "day"),
+#            lab.result = as.numeric(lab.result)) %>%
+#     inner_join(tmp.dates, by = "pie.id") %>%
+#     filter(inr.date >= first.date,
+#            inr.date <= last.date) %>%
+#     group_by(pie.id, inr.date) %>%
+#     summarize(inr = max(lab.result))
+#
+# # convert NA's to 0 before using cumsum function
+# cumsum_na <- function(x) {
+#     x[is.na(x)] <- 0
+#     cumsum(x)
+# }
+#
+# data.warfarin.courses <- tmp.warfarin.doses %>%
+#     full_join(tmp.inr, by = c("pie.id", "dose.date" = "inr.date")) %>%
+#     arrange(pie.id, dose.date) %>%
+#     mutate(new.dose = ifelse((!is.na(med.dose) & is.na(days.prev)) | days.prev > 5 |
+#                                  (days.prev > 2 & lag(inr) < 1.5), TRUE, FALSE),
+#            course = cumsum_na(new.dose))
 
 # only include patients who received at least 3 doses
 pts.screen <- raw.warfarin %>%
@@ -82,10 +81,12 @@ raw.services <- read_edw_data(dir.patients, "services_warfarin", "services") %>%
 
 # this is an ugly hack, not sure why pie.id 113105540 won't run correctly if any
 # of these 4 are in the data_frame
-tmp <- filter(raw.services, pie.id %in% c("113104277", "113105042", "113105231", "113105428")) %>%
+pie <- c("113104277", "113105042", "113105231", "113105428")
+
+tmp <- filter(raw.services, pie.id %in% pie) %>%
     tidy_data("services")
 
-tmp2 <- filter(raw.services, !(pie.id %in% c("113104277", "113105042", "113105231", "113105428"))) %>%
+tmp2 <- filter(raw.services, !(pie.id %in% pie)) %>%
     tidy_data("services")
 
 raw.services <- bind_rows(tmp, tmp2)
